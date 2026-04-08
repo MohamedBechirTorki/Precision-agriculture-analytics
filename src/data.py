c3426f210d11d484776c93a1f64ff121cbcdf061
Mohamed Bechir Torki <mohamedbechirtorki@gmail.com>
2026-04-08 14:56:17 +0100

    final

--- src/data.py
+++ src/data.py
@@ -15,21 +15,14 @@ def get_core_health_prediction_df():
 
 def get_environmental_stress_analysis_df():
     return df[[
-        "Pest_Hotspots",      # 0.85
-        "Pest_Damage",        # 0.74
-        "Thermal_Images",     # -0.67
-        "Soil_Moisture",      # -0.60
-        "Rainfall",           # -0.59
-        "Water_Flow",         # -0.57
-        "Drainage_Features",  # -0.38
-        "Organic_Matter",     # -0.26
-        "Crop_Stress_Indicator"  # target
+        "Pest_Hotspots",      
+        "Pest_Damage",        
+        "Thermal_Images",     
+        "Soil_Moisture",      
+        "Rainfall",           
+        "Water_Flow",         
+        "Drainage_Features",  
+        "Organic_Matter",     
+        "Crop_Stress_Indicator"  
     ]]
-def get_remote_sensing_UAV_analysis_df():
-    return df[["High_Resolution_RGB", "Multispectral_Images", "Thermal_Images", "Spatial_Resolution", "Bounding_Boxes", "Ground_Truth_Segmentation"]]
 
-def get_yield_prediction_df():
-    return df[["Expected_Yield", "Crop_Type", "Crop_Growth_Stage", "NDVI", "Canopy_Coverage", "Elevation_Data"]]
-
-
-print(df.corr()["Crop_Stress_Indicator"].sort_values())
